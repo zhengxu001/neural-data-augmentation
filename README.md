13dbcef38ae03fb9fa897d2e58a89005a0c516a4
Zen <zhengxu5511@gmail.com>
2018-04-21 17:07:49 +0100

    update setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Setup
+- git clone git@gitlab.scss.tcd.ie:xzheng/dissertation.git
+- cd dissertation
+- ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+mkdir dataset
 cd dataset
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech256/256_ObjectCategories.tar
 wget http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz
@@ -7,12 +7,17 @@ wget http://www.vlfeat.org/matconvnet/models/beta16/imagenet-vgg-verydeep-19.mat
 wget http://msvocds.blob.core.windows.net/coco2014/train2014.zip
 mkdir bin
 unzip train2014.zip
-tar -czvf 256_ObjectCategories.tar caltech256
-tar -czvf 101_ObjectCategories.tar.gz caltech101
 
-mkdir caltech101/train
-mkdir caltech101/val
-mv caltech101/ caltech101/train
-mkdir caltech256/train
+tar xvf 256_ObjectCategories.tar 
+rm 256_ObjectCategories.tar
+mkdir -p caltech256/train
 mkdir caltech256/val
-mv caltech256/ caltech256/train
+mv 256_ObjectCategories/* caltech256/train/
+rm -rf 256_ObjectCategories/
+
+tar xvf 101_ObjectCategories.tar.gz
+rm 101_ObjectCategories.tar.gz
+mkdir -p caltech101/train
+mkdir caltech101/val
+mv 101_ObjectCategories/* caltech101/train/
+rm -rf 101_ObjectCategories/
