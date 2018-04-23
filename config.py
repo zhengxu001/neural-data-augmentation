5936dac2c1415bce40107850ef154cfd3aa82110
Zen <zhengxu5511@gmail.com>
2018-04-23 16:00:39 +0100

    add caltech256 path

--- config.py
+++ config.py
@@ -9,9 +9,12 @@ CAL101 = os.path.join(ROOT_DIR, "dataset/caltech101")
 CAL101_TRAIN = os.path.join(CAL101, "train")
 CAL101_VAL = os.path.join(CAL101, "val")
 
-CAL101 = os.path.join(ROOT_DIR, "dataset/caltech101")
-CAL101_TRAIN_STYLE1 = os.path.join(CAL101, "train_style1")
-CAL101_VAL_STYLE1 = os.path.join(CAL101, "val_style1")
+CAL101_TRAIN_WAVE = os.path.join(DATASET, "wave/train")
+CAL101_VAL_WAVE = os.path.join(DATASET, "wave/val")
 
+
+CAL256 = os.path.join(ROOT_DIR, "dataset/caltech256")
+CAL256_TRAIN = os.path.join(CAL256, "train")
+CAL256_VAL = os.path.join(CAL256, "val")
 
 VALID_EXTS = [".jpg", ".gif", ".png", ".jpeg"]
